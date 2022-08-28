cd4e3613ff46706fda3a9fb46be44ab421384efc
BenTalese46363793 <ben.talese@students.mq.edu.au>
2022-08-28 21:29:51 +1000

    Restart

--- trackeroo/asgi.py
+++ trackeroo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PriceTracker project.
+ASGI config for trackeroo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PriceTracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trackeroo.settings')
 
 application = get_asgi_application()
